86742f43e3138b75819b38edbbea0eb7236ffd44
manish kumar <manish@appsmith.com>
2025-04-14 17:12:25 +0530

    commit for first tag

---
+++ modules/modue_version/jsobjects/modue_version/modue_version.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		return "v.0.0.1";
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
